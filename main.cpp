26532b358ddc93fcc831189422ccda8cc900d5f4
kodai <kodai@MSI>
2023-10-25 12:28:12 +0900

    01_03完成

--- main.cpp
+++ main.cpp
@@ -1,32 +1,71 @@
 #include <stdio.h>
+#include <Windows.h>
+#include <time.h>
 
-int Recursive(int n,int m)
+typedef void (*PFunc)(int*);
+
+//コールバック関数
+void DispResult(int* s)
 {
-	m--;
-	if (m <= 0)
-	{
-		return(n);
-	}
+	printf("%d秒待つ\n", *s);
+}
 
-	return (n + Recursive(n * 2 - 50,m));
-
+void setTimeout(PFunc p, int &second)
+{
+	p(&second);
+	Sleep(second * 1000);
 }
 
 int main()
 {
-	int v = 0;
-	int m = 5;
-	int n = 100;
-	int b = 1072;
+	srand((unsigned int)time(NULL));
+	
+	int n = 0;
+	int m = 3;
+	int dice;
+	dice = rand() % 6 + 1;
 
-	v = m * b;
+	PFunc p;
+	p = DispResult;
 
-	int result;
+	printf("0か1を入力してください\n");
+	scanf_s("%d",&n);
 
-	result = Recursive(n,m);
+	if (n == 0)
+	{
+		if (dice % 2 == 0)
+		{
+			setTimeout(p, m);
 
-	printf("%d時間分の再帰的な賃金体系 = %d\n", m, result);
-	printf("%d時間分の一般的な賃金体系 = %d\n", m,v);
+			printf("正解\n");
+
+		}
+		else
+		{
+			setTimeout(p, m);
+
+			printf("不正解\n");
+			
+		}
+	}
+	else
+	{
+		if (dice % 2 == 1)
+		{
+			setTimeout(p, m);
+
+			printf("正解\n");
+		}
+		else
+		{
+			setTimeout(p, m);
+
+			printf("不正解\n");
+
+		}
+	}
+
+	printf("正解は %d でした", dice);
 
 	return 0;
 }
