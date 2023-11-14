b154edb3dc0a9c3ab1a231348459719bf6bc8194
kodai <kodai@MSI>
2023-11-14 10:08:28 +0900

    未完成

--- main.cpp
+++ main.cpp
@@ -1,12 +1,41 @@
 #include <stdio.h>
-#include "Enemy.h"
+
+template<typename T>
+
+class Number
+{
+public:
+
+	T number;
+	T number2;
+
+	Number(T number,T number2) : number(number),number2(number2){}
+
+	T Min(T a,T b)
+	{
+		if (a < b)
+		{
+			return static_cast<T>(a);
+		}
+		else
+		{
+			return static_cast<T>(b);
+		}
+	}
+	
+};
 
 int main()
 {
-	Enemy enemy;
-	enemy.Update();
-	enemy.Update();
-	enemy.Update();
 
+	Number<int> b1(1, 4);
+	Number<float> b2(2.5f, 5.0f);
+	Number<double> b3(2.4f, 3.3f);
+	
+	printf("%d\n", b1);
+	printf("%f\n", b2);
+	printf("%f\n", b3);
+
+	
 	return 0;
 }
